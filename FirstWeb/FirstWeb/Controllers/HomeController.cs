259d89e278ee4ace537cf73e1652fbc314bfc89c
yf-web <yf_web@163.com>
2020-10-12 00:02:43 +0800

    项目初始化

--- FirstWeb/FirstWeb/Controllers/HomeController.cs
+++ FirstWeb/FirstWeb/Controllers/HomeController.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace FirstWeb.Controllers
 {
-    public class HomeController
+    public class HomeController:Controller
     {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+
+        public ActionResult Create()
+        {
+
+
+            return View();
+        }
 
     }
 }
--- FirstWeb/FirstWeb/Startup.cs
+++ FirstWeb/FirstWeb/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,8 +13,18 @@ namespace FirstWeb
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -23,34 +34,16 @@ namespace FirstWeb
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Use(async (httpcontext,next)=> {
-                Console.WriteLine("开始中间件A");
-                await next();
-                Console.WriteLine("退出中间件A");
-            });
-
-            app.Use(async (httpcontext, next) => {
-                Console.WriteLine("开始中间件B");
-                await next();
-                Console.WriteLine("退出中间件B");
-            });
-
+            app.UseStaticFiles();
             app.UseRouting();
 
-            app.Run(async (context) =>
-            {
-                await Task.Delay(1000);
-                context.Response.ContentType = "text/plain;charset=utf-8";
-                await context.Response.WriteAsync("中断中间件");
-            }); 
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    context.Response.ContentType = "text/palin;charset=utf-8";
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name:"default",
+                    pattern:"{controller=Home}/{action=Index}/{id?}"
+                    );
             });
         }
     }
